e649791d9693d7d3d495f789215ae1b2a729f6db
Davut Durmaz <durmaz.dvt@gmail.com>
2025-05-15 14:40:59 +0300

    Implement bootcamp 5

--- 465-Project-proper.AppHost/Program.cs
+++ 465-Project-proper.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.API_Movie>("api-movie");
 
+builder.AddProject<Projects.API_Users>("api-users");
+
 builder.Build().Run();
